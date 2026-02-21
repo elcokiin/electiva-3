b7b620d41e1383d272ff32c59f6faa49d27e23e4
Diego Tenjo <tenjodiego961@gmail.com>
2026-02-21 15:30:00 -0500

    feat(service-worker): Implement random image swapping

--- src/sw.js
+++ src/sw.js
@@ -1,10 +1,15 @@
-let useCat = false;
+const images = ["cat.svg", "dog.svg", "horse.svg", "rabbit.svg"];
+let currentImage = "dog.svg";
+let shouldSwap = false;
+let swappedImage = null;
 
 self.addEventListener("install", (event) => {
   console.log("Service worker installing....");
 
   event.waitUntil(
-    caches.open("chaceApp").then((cache) => cache.add("cat.svg")),
+    caches
+      .open("cacheApp")
+      .then((cache) => cache.addAll(images)),
   );
 });
 
@@ -13,15 +18,28 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("message", (event) => {
-  if (event.data.action === "switchToCat") {
-    useCat = true;
+  if (event.data.action === "swap") {
+    currentImage = event.data.current;
+    const others = images.filter((img) => img !== currentImage);
+    swappedImage = others[Math.floor(Math.random() * others.length)];
+    currentImage = swappedImage;
+    shouldSwap = true;
+
+    // Responder al cliente con la imagen elegida
+    event.source.postMessage({
+      action: "swapped",
+      image: swappedImage,
+    });
   }
 });
 
 self.addEventListener("fetch", (e) => {
   const url = new URL(e.request.url);
-  if (url.pathname.includes("dog.svg") && useCat) {
-    e.respondWith(caches.match("cat.svg"));
+  const isAnimalImage = images.some((img) => url.pathname.includes(img));
+
+  if (isAnimalImage && shouldSwap) {
+    shouldSwap = false;
+    e.respondWith(caches.match(swappedImage));
   } else {
     e.respondWith(fetch(e.request));
   }
